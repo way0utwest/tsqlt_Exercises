91536014830ea0d1cff0d928c76b73bfdf3c5606
way0utwest <sjones@dkranch.net>
2015-10-21 13:59:05 -0600

    Added nocount to the proc as per standards and to prevent extra messages to the client

--- Stored Procedures/dbo.SendBirthdayEmail.sql
+++ Stored Procedures/dbo.SendBirthdayEmail.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -7,6 +8,7 @@ CREATE PROC [dbo].[SendBirthdayEmail]
   @CustomerId INT
 AS
 BEGIN
+  SET NOCOUNT ON 
   RAISERROR('Don''t you dare call me again! ;-)',16,10);
 END;
 GO
